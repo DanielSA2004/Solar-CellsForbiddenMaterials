7ce8b54a940e4aaa4656efc10b2eeda33a00550e
DanielSA2004 <dsilvaa@unicartagena.edu.co>
2025-10-03 21:42:52 -0500

    Flutter-update

---
+++ lib/Frontend/appmain.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:fl_chart/fl_chart.dart';
+
+class MymainApp extends StatefulWidget {
+  const MymainApp({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MymainApp> createState() => _MymainAppState();
+}
+
+class _MymainAppState extends State<MymainApp> {
+  
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        
+      );
+  }
+}
